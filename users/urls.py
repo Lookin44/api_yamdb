c0503991b6adef75c291abdf2b668a0d85ffac2b
Egor <look-in44@yandex.ru>
2021-02-16 19:08:25 +0300

    Pass 4 first PyTests

---
+++ users/urls.py
@@ -0,0 +1,13 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+from .views import UserViewSet
+
+router_v1 = DefaultRouter()
+router_v1.register('users', UserViewSet, basename="Users")
+
+urlpatterns = [
+    path('v1/', include(router_v1.urls)),
+    path('v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+]
