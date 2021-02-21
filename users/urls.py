d27d459e06e517b28e72a8bb7724ec224f98f681
Egor <look-in44@yandex.ru>
2021-02-21 12:15:48 +0300

    Added simple JWT authorization for manual tests

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (
+        TokenObtainPairView,
+        TokenRefreshView,
+    )
 
 from .views import UserViewSet, UserInfo
 
@@ -7,6 +11,10 @@ router_v1 = DefaultRouter()
 router_v1.register('users', UserViewSet, basename='users')
 
 urlpatterns = [
+    path('v1/token/', TokenObtainPairView.as_view(),
+         name='token_obtain_pair'),
+    path('v1/token/refresh/', TokenRefreshView.as_view(),
+         name='token_refresh'),
     path('v1/users/me/', UserInfo.as_view()),
     path('v1/', include(router_v1.urls)),
 ]
