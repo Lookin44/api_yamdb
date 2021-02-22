8ed45252eab7fbf0a10038d46ee9894bebe0d7c7
Egor <look-in44@yandex.ru>
2021-02-22 15:18:47 +0300

    Added def confirmation_code_sender

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from .views import UserViewSet, UserInfo
+from .views import UserViewSet, UserInfo, confirmation_code_sender
 
 router_v1 = DefaultRouter()
 router_v1.register('users', UserViewSet, basename='users')
 
 urlpatterns = [
+    path('v1/auth/email/', confirmation_code_sender),
     path('v1/users/me/', UserInfo.as_view()),
     path('v1/', include(router_v1.urls)),
 ]
--- users/views.py
+++ users/views.py
@@ -1,24 +1,41 @@
+import uuid
+
+from django.core.mail import send_mail
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, authentication_classes, \
+    permission_classes
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
+from api_yamdb import settings
 from .models import User
 from .permissions import AdminPermission
 from .serializers import UserSerializer, EmailSerializer, TokenSerializer
 
 
-# @api_view(['POST'])
-# def confirmation_code(request):
-#     serializer = EmailSerializer(data=request.data)
-#     serializer.is_valid(raise_exception=True)
-#
-#     email = serializer.data['email']
-#     user_in_database = User.objects.filter(email=email).exists()
-#     if not user_in_database:
-#         username =
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def confirmation_code_sender(request):
+    serializer = EmailSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+
+    email = serializer.data['email']
+    user_is_exist = User.objects.filter(email=email).exists()
+    if not user_is_exist:
+        User.objects.create_user(username=email, email=email)
+
+    confirmation_code = uuid.uuid3(uuid.NAMESPACE_DNS, email)
+
+    send_mail(
+        subject='Ваш персональный код от сервиса YamDb',
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        message=f'Ваш код: {confirmation_code}, он будет действовать 24 часа.',
+        recipient_list=[email],
+        fail_silently=False,
+    )
+    return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 class UserViewSet(ModelViewSet):
@@ -43,4 +60,3 @@ class UserInfo(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
